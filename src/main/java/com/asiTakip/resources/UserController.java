2d7b4c335fab9fed3ed78de824d9f5c334b5615e
BEDRETTİN AKYÜZ <bedrettinakyuz@github.com>
2021-10-11 13:03:09 +0300

    connection

--- src/main/java/com/asiTakip/resources/UserController.java
+++ src/main/java/com/asiTakip/resources/UserController.java
@@ -14,7 +14,7 @@ import com.asiTakip.Business.IUserManager;
 @RequestMapping("/user")
 public class UserController {
 
-    @RequestMapping("/")
+	@GetMapping
     public String index() {
         return "Clever-Cloud demo of java with maven, spring-boot and mongodb.";
     }
